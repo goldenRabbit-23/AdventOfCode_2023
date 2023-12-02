b228f4e28a2f5850f8c40e3e04353b6deb4d8e0c
justin <goldrabbit0926@gmail.com>
2023-12-02 17:15:12 +0900

    finished day 02

---
+++ day02/p1.py
@@ -0,0 +1,36 @@
+import sys
+
+lines = open(sys.argv[1]).readlines()
+ans = 0
+game = 1
+
+for line in lines:
+    flag = True
+
+    line = line.strip()
+    body = line.split(': ')[1]
+    sets = body.split('; ')
+
+    for set in sets:
+        cubes = set.split(', ')
+        red = green = blue = 0
+
+        for cube in cubes:
+            count, color = cube.split(' ')
+            if color == 'red':
+                red = int(count)
+            if color == 'green':
+                green = int(count)
+            if color == 'blue':
+                blue = int(count)
+
+        if red > 12 or green > 13 or blue > 14:
+            flag = False
+            break
+
+    if flag:
+        ans += game
+
+    game += 1
+
+print(ans)
---
+++ day02/p2.py
@@ -0,0 +1,31 @@
+import sys
+
+lines = open(sys.argv[1]).readlines()
+ans = 0
+
+for line in lines:
+    line = line.strip()
+    body = line.split(': ')[1]
+    sets = body.split('; ')
+
+    max_red = max_green = max_blue = 0
+    for set in sets:
+        cubes = set.split(', ')
+        red = green = blue = 0
+
+        for cube in cubes:
+            count, color = cube.split(' ')
+            if color == 'red':
+                red = int(count)
+            if color == 'green':
+                green = int(count)
+            if color == 'blue':
+                blue = int(count)
+
+            max_red = max(max_red, red)
+            max_green = max(max_green, green)
+            max_blue = max(max_blue, blue)
+
+    ans += max_red * max_green * max_blue
+
+print(ans)
