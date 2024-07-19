3d2e42bb15c5c69750a706c70e5fecf5d896e8b7
justin <goldrabbit0926@gmail.com>
2024-07-20 02:09:18 +0900

    changed indentation in day 06

--- day06/p2.py
+++ day06/p2.py
@@ -7,14 +7,14 @@ dist = int(''.join(map(str, list(map(int, dists.split()[1:])))))
 
 small_root = (time - sqrt(time ** 2 - 4 * dist)) / 2
 if small_root.is_integer():
-    min_time = int(small_root + 1)
+  min_time = int(small_root + 1)
 else:
-    min_time = ceil(small_root)
+  min_time = ceil(small_root)
 
 center = time / 2
 if center.is_integer():
-    res = 2 * int(center - min_time) + 1
+  res = 2 * int(center - min_time) + 1
 else:
-    res = 2 * (ceil(center) - min_time)
+  res = 2 * (ceil(center) - min_time)
 
 print(res)
