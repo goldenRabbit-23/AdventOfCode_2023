1be9da24bd898249a0ae54b2c8f1fc86ba4d160b
justin <goldrabbit0926@gmail.com>
2024-07-20 02:09:37 +0900

    changed indentation in day 08

--- day08/p2.py
+++ day08/p2.py
@@ -6,26 +6,26 @@ instr = data[0]
 nodes = {}
 
 for item in data[2:]:
-    key, value = item.split(' = ')
-    value = value.strip('()')
-    l, r = value.split(', ')
-    nodes[key] = (l, r)
+  key, value = item.split(' = ')
+  value = value.strip('()')
+  l, r = value.split(', ')
+  nodes[key] = (l, r)
 
 starts = [key for key in nodes.keys() if key[-1] == 'A']
 
 
 def calc(start):
-    cur = start
-    steps = 0
-    while True:
-        for dir in instr:
-            if dir == 'L':
-                cur = nodes[cur][0]
-            else:
-                cur = nodes[cur][1]
-            steps += 1
-            if cur[-1] == 'Z':
-                return steps
+  cur = start
+  steps = 0
+  while True:
+    for dir in instr:
+      if dir == 'L':
+        cur = nodes[cur][0]
+      else:
+        cur = nodes[cur][1]
+      steps += 1
+      if cur[-1] == 'Z':
+        return steps
 
 
 print(lcm(*map(calc, starts)))
