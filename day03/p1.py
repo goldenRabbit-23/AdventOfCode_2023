c4e8f7e6fb2e2336cd8bd064bcf30772b58254df
justin <goldrabbit0926@gmail.com>
2024-07-20 02:08:39 +0900

    changed indentation in day 03

--- day03/p1.py
+++ day03/p1.py
@@ -6,26 +6,26 @@ flag = False
 res = 0
 
 for i in range(len(grid)):
-    for j in range(len(grid[i]) + 1):
-        if j == len(grid[i]):
-            if num_str != "" and flag:
-                res += int(num_str)
-                flag = False
-            num_str = ""
-        elif num_str == "" and not grid[i][j].isnumeric():
-            continue
-        elif num_str != "" and not grid[i][j].isnumeric():
-            if flag:
-                res += int(num_str)
-                flag = False
-            num_str = ""
-        else:
-            num_str += grid[i][j]
-            for dx, dy in [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]]:
-                if i + dx < 0 or i + dx >= len(grid) or j + dy < 0 or j + dy >= len(grid[i]):
-                    continue
-                if not grid[i + dx][j + dy].isnumeric() and grid[i + dx][j + dy] != '.':
-                    flag = True
-                    break
+  for j in range(len(grid[i]) + 1):
+    if j == len(grid[i]):
+      if num_str != "" and flag:
+        res += int(num_str)
+        flag = False
+      num_str = ""
+    elif num_str == "" and not grid[i][j].isnumeric():
+      continue
+    elif num_str != "" and not grid[i][j].isnumeric():
+      if flag:
+        res += int(num_str)
+        flag = False
+      num_str = ""
+    else:
+      num_str += grid[i][j]
+      for dx, dy in [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]]:
+        if i + dx < 0 or i + dx >= len(grid) or j + dy < 0 or j + dy >= len(grid[i]):
+          continue
+        if not grid[i + dx][j + dy].isnumeric() and grid[i + dx][j + dy] != '.':
+          flag = True
+          break
 
 print(res)
--- day03/p2.py
+++ day03/p2.py
@@ -8,38 +8,38 @@ found = False
 res = 0
 
 for i in range(len(grid)):
-    for j in range(len(grid[i])):
-        if grid[i][j] == '*':
-            gears[(i, j)]
+  for j in range(len(grid[i])):
+    if grid[i][j] == '*':
+      gears[(i, j)]
 
 for i in range(len(grid)):
-    for j in range(len(grid[i]) + 1):
-        if j == len(grid[i]):
-            if num_str != "" and found:
-                gears[(x, y)].append(int(num_str))
-                found = False
-            num_str = ""
-            x, y = 0, 0
-        elif num_str == "" and not grid[i][j].isnumeric():
-            continue
-        elif num_str != "" and not grid[i][j].isnumeric():
-            if found:
-                gears[(x, y)].append(int(num_str))
-                found = False
-            num_str = ""
-            x, y = 0, 0
-        else:
-            num_str += grid[i][j]
-            for dx, dy in [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]]:
-                if i + dx < 0 or i + dx >= len(grid) or j + dy < 0 or j + dy >= len(grid[i]):
-                    continue
-                if grid[i + dx][j + dy] == '*':
-                    found = True
-                    x, y = i + dx, j + dy
-                    break
+  for j in range(len(grid[i]) + 1):
+    if j == len(grid[i]):
+      if num_str != "" and found:
+        gears[(x, y)].append(int(num_str))
+        found = False
+      num_str = ""
+      x, y = 0, 0
+    elif num_str == "" and not grid[i][j].isnumeric():
+      continue
+    elif num_str != "" and not grid[i][j].isnumeric():
+      if found:
+        gears[(x, y)].append(int(num_str))
+        found = False
+      num_str = ""
+      x, y = 0, 0
+    else:
+      num_str += grid[i][j]
+      for dx, dy in [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]]:
+        if i + dx < 0 or i + dx >= len(grid) or j + dy < 0 or j + dy >= len(grid[i]):
+          continue
+        if grid[i + dx][j + dy] == '*':
+          found = True
+          x, y = i + dx, j + dy
+          break
 
 for parts in gears.values():
-    if len(parts) == 2:
-        res += parts[0] * parts[1]
+  if len(parts) == 2:
+    res += parts[0] * parts[1]
 
 print(res)
