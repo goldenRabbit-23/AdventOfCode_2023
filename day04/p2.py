b811eced2ae1e5a2fc2d19cfdcfcd69343e5655a
justin <goldrabbit0926@gmail.com>
2024-07-20 02:08:55 +0900

    changed indentation in day 04

--- day04/p2.py
+++ day04/p2.py
@@ -5,20 +5,20 @@ wins = []
 card_count = [1 for _ in range(len(cards))]
 
 for card in cards:
-    card = card.split(': ')[1]
-    win_num, my_num = card.split(' | ')
-    win_num = win_num.split()
-    my_num = my_num.split()
+  card = card.split(': ')[1]
+  win_num, my_num = card.split(' | ')
+  win_num = win_num.split()
+  my_num = my_num.split()
 
-    win_cnt = 0
-    for num in my_num:
-        if num in win_num:
-            win_cnt += 1
+  win_cnt = 0
+  for num in my_num:
+    if num in win_num:
+      win_cnt += 1
 
-    wins.append(win_cnt)
+  wins.append(win_cnt)
 
 for i in range(len(cards)):
-    for j in range(i+1, i+1+wins[i]):
-        card_count[j] += card_count[i]
+  for j in range(i+1, i+1+wins[i]):
+    card_count[j] += card_count[i]
 
 print(sum(card_count))
