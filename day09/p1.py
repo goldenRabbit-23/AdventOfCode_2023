f0ada0201241021990f869febc460c7bc82382c8
justin <goldrabbit0926@gmail.com>
2024-07-20 02:09:47 +0900

    changed indentation in day 09

--- day09/p1.py
+++ day09/p1.py
@@ -4,16 +4,16 @@ data = open(sys.argv[1]).read().splitlines()
 res = 0
 
 for line in data:
-    history = list(map(int, line.split()))
-    last_value = history[-1]
+  history = list(map(int, line.split()))
+  last_value = history[-1]
 
-    while not all(h == 0 for h in history):
-        diff = []
-        for i in range(len(history) - 1):
-            diff.append(history[i + 1] - history[i])
-        last_value += diff[-1]
-        history = diff
+  while not all(h == 0 for h in history):
+    diff = []
+    for i in range(len(history) - 1):
+      diff.append(history[i + 1] - history[i])
+    last_value += diff[-1]
+    history = diff
 
-    res += last_value
+  res += last_value
 
 print(res)
--- day09/p2.py
+++ day09/p2.py
@@ -5,17 +5,17 @@ res = 0
 
 
 def extrapolate(history):
-    if all(h == 0 for h in history):
-        return 0
+  if all(h == 0 for h in history):
+    return 0
 
-    diff = []
-    for i in range(len(history) - 1):
-        diff.append(history[i] - history[i + 1])
-    return history[-1] - extrapolate(diff)
+  diff = []
+  for i in range(len(history) - 1):
+    diff.append(history[i] - history[i + 1])
+  return history[-1] - extrapolate(diff)
 
 
 for line in data:
-    history = list(map(int, line.split()))
-    res += extrapolate(history[::-1])
+  history = list(map(int, line.split()))
+  res += extrapolate(history[::-1])
 
 print(res)
